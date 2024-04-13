8c05b3dd41823e5f394927b80aff5cb3a4b32e33
Alexandru Edves <95491924+AlexE10@users.noreply.github.com>
2024-04-13 15:54:33 +0300

    Created game scene, adaptive power for shots and cars controller

--- Volleyball 3D/Assets/Movement/Scripts/ShotManager.cs
+++ Volleyball 3D/Assets/Movement/Scripts/ShotManager.cs
@@ -7,33 +7,53 @@ public class ShotManager : MonoBehaviour
     [SerializeField] public GameObject ball;
     [SerializeField] private Camera playerCamera;
     [SerializeField] private GameObject lowkickHelper;
+    [SerializeField, Range(1, 20)] private float lowkickRange;
+    [SerializeField, Range(1, 20)] private float upperkickRange;
+    [SerializeField] private float reflectedPointHeight;
+    [SerializeField] private float pathHeightForUpperkick;
 
     private Rigidbody ballRigidbody;
 
+    private float previousDistance;
+    private Vector3 playerPositionReflectedToNet;
+
     private void Start()
     {
-        ballRigidbody = ball.GetComponent<Rigidbody>(); 
+        ballRigidbody = ball.GetComponent<Rigidbody>();
+        //playerPositionReflectedToNet = transform.position;
+        //playerPositionReflectedToNet.z = 0f;
+        //previousDistance = Vector3.Distance(transform.position, playerPositionReflectedToNet);
     }
 
     public void Lowkick()
     {
-        Ray viewDirectionRay = new Ray(transform.position, playerCamera.transform.forward);
-        if (lowkickHelper.GetComponent<BoxCollider>().Raycast(viewDirectionRay, out RaycastHit hit, Mathf.Infinity))
+        if (Vector3.Distance(transform.position, ball.transform.position) < lowkickRange)
         {
-            Vector3 updatedHit = hit.point;
-            updatedHit.y = 50;
-            updatedHit -= transform.position;
+            AdaptLowkickPower();
 
-            ballRigidbody.AddForce(updatedHit * lowkickPower);
+            Ray viewDirectionRay = new Ray(transform.position, playerCamera.transform.forward);
+            if (lowkickHelper.GetComponent<BoxCollider>().Raycast(viewDirectionRay, out RaycastHit hit, Mathf.Infinity))
+            {
+                Vector3 updatedHit = hit.point;
+                updatedHit.y = reflectedPointHeight;
+                updatedHit -= transform.position;
+
+                ballRigidbody.AddForce(updatedHit * lowkickPower);
+            }
         }
     }
 
     public void UpperKick()
     {
-        Vector3 offset = new Vector3(0, 0.1f, 0);
-        Vector3 upperkickDirection = GetReflected(offset) + new Vector3(0, 0.3f, 0);
+        if (Vector3.Distance(transform.position, ball.transform.position) < upperkickRange)
+        {
+            AdaptUpperkickPower();
 
-        ballRigidbody.velocity = upperkickDirection * upperkickPower;
+            Vector3 offset = new Vector3(0, 0.1f, 0);
+            Vector3 upperkickDirection = GetReflected(offset) + new Vector3(0, pathHeightForUpperkick, 0);
+
+            ballRigidbody.velocity = upperkickDirection * upperkickPower;
+        }
     }
 
     private Vector3 GetReflected(Vector3 offset)
@@ -41,12 +61,55 @@ public class ShotManager : MonoBehaviour
         Vector3 cameraOffset = playerCamera.transform.forward + offset;
         cameraOffset.x = 0f;
         cameraOffset.z = 0f;
-        Debug.Log(cameraOffset);
         Vector3 volleyballlVector = transform.position - ball.transform.position;
         Vector3 planeTangent = Vector3.Cross(volleyballlVector, cameraOffset);
         Vector3 planeNormal = Vector3.Cross(planeTangent, volleyballlVector);
         Vector3 reflected = Vector3.Reflect(cameraOffset, planeNormal);
 
         return reflected.normalized;
+    }
+
+    private void AdaptLowkickPower()
+    {
+        playerPositionReflectedToNet = transform.position;
+        playerPositionReflectedToNet.z = 0;
+
+        float currentDistance = Vector3.Distance(transform.position, playerPositionReflectedToNet);
+
+        if (currentDistance - previousDistance > 1)
+        {
+            reflectedPointHeight -= 2;
+            lowkickPower++;
+        }
+        else if (currentDistance - previousDistance < -1)
+        {
+            reflectedPointHeight += 2;
+            lowkickPower--;
+        }
+
+        previousDistance = currentDistance;
+
+        Debug.Log(reflectedPointHeight);
+    }
+
+    private void AdaptUpperkickPower()
+    {
+        playerPositionReflectedToNet = transform.position;
+        playerPositionReflectedToNet.z = 0;
+
+        float currentDistance = Vector3.Distance(transform.position, playerPositionReflectedToNet);
+
+        if (currentDistance - previousDistance > 1)
+        {
+            pathHeightForUpperkick += 0.1f;
+            upperkickPower--;
+        }
+        else if (currentDistance - previousDistance < -1)
+        {
+            pathHeightForUpperkick -= 0.1f;
+            upperkickPower++;   
+        }
+
+        previousDistance = currentDistance;
     }
 }
